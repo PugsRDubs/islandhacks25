b3442277ca18bfb1c60c907afd60f587be1f9bc8
PugsRDubs <138230634+PugsRDubs@users.noreply.github.com>
2025-11-15 13:28:13 -0800

    Update Timer.svelte

--- src/routes/Timer.svelte
+++ src/routes/Timer.svelte
@@ -8,12 +8,46 @@
 
 <script lang="ts">
     let { timer } = $props();
+
+    let seconds: number = $state(timer.time);
+
+    let counting = $state(false);
+
+    const makeIteration = (): void => {
+        if (seconds > 1) {
+            if (counting) {
+                setTimeout(makeIteration, 1000); // 1 second waiting
+            }
+        }
+        if (counting) {
+            seconds -= 1;
+        }
+    };
+
+    function timerClicked() {
+        if (counting) {
+            counting = false;
+        }
+        else {
+            counting = true;
+            setTimeout(makeIteration, 1000);
+        }
+    }
+
+    function reset() {
+        counting=false; 
+        seconds=timer.time;
+    }
+    
 </script>
 
-<li>timer</li>
+<button onclick={() => timerClicked()}>{#if counting}pause{:else}start{/if}</button>
 <button onclick={() => timer.hidden = !timer.hidden}>{#if timer.hidden}show{:else}hide{/if}</button>
+{#if seconds != timer.time || counting}
+<button onclick={reset}>reset</button>
+{/if}
 {#if !timer.hidden}
-    <p>{timer.time}</p>
+    <p>{seconds}</p>
 {/if}
 
 <style></style>
