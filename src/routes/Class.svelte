f77b06d7377bab6671a3dc726b2ba8f3f1726909
PugsRDubs <138230634+PugsRDubs@users.noreply.github.com>
2025-11-15 13:51:54 -0800

    Create Class.svelte

---
+++ src/routes/Class.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { TimerData } from "./Timer.svelte";
+    import Timer from "./Timer.svelte";
+
+    var timers : Array<TimerData> = $state([]);
+
+    function a() {
+        timers.push({ name: "Timer", time: 10, hidden: true});
+    }
+</script>
+
+<h1>English</h1>
+<button onclick={a}>abcd</button>
+<ul>
+    {#each timers as timer, i}
+        <Timer {timer} />
+    {/each}
+</ul>
