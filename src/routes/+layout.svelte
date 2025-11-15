fe82546c5ff5c3149fc9758f0cea2d5efa4e21e7
tttdban <tdbanthi@gmail.com>
2025-11-15 14:06:07 -0800

    modified layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,18 +19,22 @@
 <style>
 	:global(body) {
 		background-color: rgb(45, 47, 50);
+		text-align: center;
+		color:white
 	}
 	h1{
 		text-align: center;
-		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-family: 'Gill Sans', 'Gill Sans MT', 'Calibri', 'Trebuchet MS', sans-serif;
 		font-size: 60px;
 		color: rgba(235, 235, 243, 0.85);
 	}
 	div {
-		margin: 20px;
-		padding: 10px;
+		margin: 40px;
 		background-color: rgb(45, 47, 50);
+		border-style: solid;
+		border-width: 5px;
 		border-radius: 5px;
+		border-color:rgb(70, 70, 72);
 
 	}
 </style>
